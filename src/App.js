b2c599605c1b4201bf7a9fb7d70ae8ba027db5ac
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-06-05 19:56:24 +0300

    redirect error fixed

--- src/App.js
+++ src/App.js
@@ -25,20 +25,28 @@ function App() {
             <span className="font-bold text-2xl">Friends Database</span>
           </Link>
           <div className="flex gap-x-5">
-            <Link to="/friends" className="border px-5 py-2 hover:bg-gray-400">
-              Friend List
-            </Link>
-            <Link to="/friends/add" className="border px-5 py-2 hover:bg-gray-400">
-              Add Friend
-            </Link>
-            <Link to={isLoggedIn ? "/logout" : "/login"} className="border px-5 py-2 hover:bg-gray-400">
-              {isLoggedIn ? "Logout" : "Login"}
-            </Link>
+            {isLoggedIn ? (
+              <>
+                <Link to="/friends" className="border px-5 py-2 hover:bg-gray-400">
+                  Friend List
+                </Link>
+                <Link to="/friends/add" className="border px-5 py-2 hover:bg-gray-400">
+                  Add Friend
+                </Link>
+                <Link to="/logout" className="border px-5 py-2 hover:bg-gray-400">
+                  Logout
+                </Link>
+              </>
+            ) : (
+              <Link to="/login" className="border px-5 py-2 hover:bg-gray-400">
+                Login
+              </Link>
+            )}
           </div>
         </div>
       </div>
       <Route exact path="/login">
-        {isLoggedIn ? <Redirect path to="/friends" replace /> : <Login />}
+        <Login />
       </Route>
       <Route exact path="/logout">
         <Logout />
